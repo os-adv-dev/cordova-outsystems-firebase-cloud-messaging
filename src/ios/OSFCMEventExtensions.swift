06ef07363802d0857f5ff4200ed7a59537e0ee91
Ricardo Silva <97543217+OS-ricardomoreirasilva@users.noreply.github.com>
2023-07-31 15:00:17 +0100

    fix: iOS | Trigger "NotificationClickV2" (#71)
    Update the iOS library to use the latest version that enables the correct triggering of the new NotificationClick event.
    Update the implementation of the "FirebaseMessagingEventProtocol" to trigger multiple events simultaneously, including the new NotificationClick event.
    Extend OSFCMClickableType and FirebaseNotificationType to implement the CustomStringConvertible protocol, applying the correct value to the description property.

---
+++ src/ios/OSFCMEventExtensions.swift
@@ -0,0 +1,24 @@
+import OSFirebaseMessagingLib
+
+extension OSFCMClickableType: CustomStringConvertible {
+    public var description: String {
+        var result: String
+        switch self {
+        case .notification(latestVersion: let latestVersion):
+            result = "notificationClick"
+            if latestVersion {
+                result += "V2"
+            }
+        case .action:
+            result = "interalRouteActionClick"
+        @unknown default:
+            preconditionFailure("Not supposed to get here")
+        }
+        
+        return result
+    }
+}
+
+extension FirebaseNotificationType: CustomStringConvertible {
+    public var description: String { "\(self == .silentNotification ? "silent": "default")Notification" }
+}
